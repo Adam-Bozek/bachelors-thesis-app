3ffdff408291bff829882ed7f86fd42fa9550955
Adam Božek <adamoss12323@gmail.com>
2024-02-28 14:24:34 +0100

    function to change svg icon when clicked

--- client-app/src/components/private/game/Tiles.js
+++ client-app/src/components/private/game/Tiles.js
@@ -1,6 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Style from "./styles/Style.module.css";
 
+import { ReactComponent as PlaySVG } from "./svg/play.svg";
+import { ReactComponent as PauseSVG } from "./svg/pause.svg";
+
+{/* One title  */}
 const Tile = ({ imageUrl, onClick, index }) => (
     <div className="col-md-4 mb-3">
         <div className="card clickable text-center" onClick={() => onClick(index)}> 
@@ -12,19 +16,25 @@ const Tile = ({ imageUrl, onClick, index }) => (
 );
 
 const Tiles = ({ question, imgLinks }) => {
-    const handleClick = (index) => { 
-        alert("you clicked tile number " + (index + 1)); 
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    const handleTileClick = (index) => { 
+        alert("you clicked tile number " + (index + 1));
+    };
+
+    const handlePausePlayClick = () => {
+        setIsPlaying(!isPlaying);
     };
 
     return (
         <div className="container" data-bs-theme="light">
-            <h1 className="py-4">{question}</h1>
+            <h1 className="py-4"> {question} {isPlaying ? <PauseSVG onClick={handlePausePlayClick}/> : <PlaySVG onClick={handlePausePlayClick}/>} </h1>
             <div className="row">
                 {imgLinks.map((imgLink, index) => (
                     <Tile
                         key={index}
                         imageUrl={imgLink}
-                        onClick={handleClick}
+                        onClick={handleTileClick}
                         index={index} 
                     />
                 ))}
