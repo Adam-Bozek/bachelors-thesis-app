d2289f8b659d1077d94d26fea327f3a85a8ad8ac
Adam Božek <adamoss12323@gmail.com>
2024-03-05 16:06:21 +0100

    commented code

--- client-app/src/components/private/game/Controller.js
+++ client-app/src/components/private/game/Controller.js
@@ -6,13 +6,17 @@ import O_A_BP_FaD from "./data/questions/O_A_BP_FaD.json";
 
 // Function to dynamically import images
 const importAll = (require) => {
+  // Initialize an empty object to store the imported images
   let images = {};
+  // Using require.context to get all image files from the specified directory
   require.keys().map((item, index) => {
+    // Store each imported image in the images object with its filename as the key
     images[item.replace("./", "")] = require(item);
   });
   return images;
 };
 
+// Call the importAll function to import all images from the specified directory
 const images = importAll(
   require.context("./data/pictures", false, /\.(png|jpe?g|svg|webp)$/)
 );
@@ -20,16 +24,19 @@ const images = importAll(
 const Controller = () => {
   const [slides, setSlides] = useState([]);
 
+  // Function to parse the JSON data
   const jsonData = () => JSON.parse(JSON.stringify(O_A_BP_FaD));
 
   useEffect(() => {
     fetchQuestions();
   }, []);
 
+  // Function to fetch questions asynchronously
   const fetchQuestions = async () => {
     try {
       const data = jsonData();
 
+      // Create an array of slides by mapping over the 'marketplace' array in the JSON data
       const slidesArray = data.marketplace.map((item) => ({
         question: item.question,
         imgLinks: item.answers.map(
@@ -38,8 +45,7 @@ const Controller = () => {
         ),
       }));
       setSlides(slidesArray);
-    } catch (error) {
-      console.error("Error fetching questions:", error);
+    } catch (error) {      console.error("Error fetching questions:", error);
     }
   };
 
