14568940ab6121b62c549afcaeb095885ed57931
Adam Božek <adamoss12323@gmail.com>
2024-03-31 13:52:10 +0200

    controller component now displays questions from different categories

--- client-app/src/components/private/game/Controller.js
+++ client-app/src/components/private/game/Controller.js
@@ -8,53 +8,64 @@ import O_A_BP_FaD from "./data/json/O_A_BP_FaD.json";
 const Controller = () => {
   const jsonData = () => JSON.parse(JSON.stringify(O_A_BP_FaD));
 
+  const categories = ["marketplace", "mountains", "zoo", "home", "street"];
+
+  const [currentCategoryIndex, setCurrentCategoryIndex] = useState(0);
   const [isActive, setIsActive] = useState(false);
 
-  // State to store answers for ImageRecognition component
-  const [recognitionAnswers, setRecognitionAnswers] = useState([]);
-  // State to store answers for ImageSelection component
+  // Define state variables and their updater functions for selection and recognition answers
   const [selectionAnswers, setSelectionAnswers] = useState([]);
+  const [recognitionAnswers, setRecognitionAnswers] = useState([]);
 
   const receiveUserAnswers = (answers, componentType) => {
-    // Store answers in different arrays based on the component type
-    if (componentType === "ImageRecognition") {
-      setRecognitionAnswers((prevAnswers) => [...prevAnswers, answers]);
-      console.log("recognitionAnswers:");
-      console.log(recognitionAnswers);
-      setIsActive(false);
-    } else if (componentType === "ImageSelection") {
-      setSelectionAnswers((prevAnswers) => [...prevAnswers, answers]);
-      console.log("selectionAnswers:");
-      console.log(selectionAnswers);
+    // Store answers based on the current category index
+    const category = categories[currentCategoryIndex];
+    if (componentType.endsWith("ImageSelection")) {
+      setSelectionAnswers((prevAnswers) => [
+        ...prevAnswers,
+        { category, answers },
+      ]);
       setIsActive(true);
+    } else if (componentType.endsWith("ImageRecognition")) {
+      setRecognitionAnswers((prevAnswers) => [
+        ...prevAnswers,
+        { category, answers },
+      ]);
+      setIsActive(false);
+      // Move to the next category after completing recognition
+      setCurrentCategoryIndex((prevIndex) => prevIndex + 1);
     }
   };
 
   return (
     <>
-      {!isActive ? (
+      {currentCategoryIndex < categories.length && (
         <>
-          {/* Render ImageSelection component */}
-          <ImageSelection
-            filename={"O_A_BP_FaD"}
-            category={"marketplace"}
-            jsonData={jsonData}
-            receiveUserAnswers={(answers) =>
-              receiveUserAnswers(answers, "ImageSelection")
-            }
-          />
-        </>
-      ) : (
-        <>
-          {/* Render ImageRecognition component */}
-          <ImageRecognition
-            filename={"O_A_BP_FaD"}
-            category={"marketplace"}
-            jsonData={jsonData}
-            receiveUserAnswers={(answers) =>
-              receiveUserAnswers(answers, "ImageRecognition")
-            }
-          />
+          {!isActive ? (
+            <ImageSelection
+              filename={"O_A_BP_FaD"}
+              category={categories[currentCategoryIndex]}
+              jsonData={jsonData}
+              receiveUserAnswers={(answers) =>
+                receiveUserAnswers(
+                  answers,
+                  `${categories[currentCategoryIndex]}ImageSelection`
+                )
+              }
+            />
+          ) : (
+            <ImageRecognition
+              filename={"O_A_BP_FaD"}
+              category={categories[currentCategoryIndex]}
+              jsonData={jsonData}
+              receiveUserAnswers={(answers) =>
+                receiveUserAnswers(
+                  answers,
+                  `${categories[currentCategoryIndex]}ImageRecognition`
+                )
+              }
+            />
+          )}
         </>
       )}
     </>
