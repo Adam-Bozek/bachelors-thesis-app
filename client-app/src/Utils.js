102965fe1bc6070bded0aeac0b6f15df22e78ea0
Adam Božek <adamoss12323@gmail.com>
2024-05-25 14:33:33 +0200

    changing api address to env variable

--- client-app/src/Utils.js
+++ client-app/src/Utils.js
@@ -5,7 +5,7 @@ import Axios from "axios";
 const apiKey = process.env.REACT_APP_API_KEY;
 
 export const webProtocol = "https";
-export const apiIPAddress = "localhost:3001";
+export const apiIPAddress = process.env.REACT_APP_API_ADDRESS;
 
 export const year = new Date().getFullYear();
 
